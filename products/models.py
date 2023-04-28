7fd5089291e3911cdecfec18db28129239783a46
henriits <121551949+henriits@users.noreply.github.com>
2023-04-28 20:33:40 +0300

    register post in admin, create post class

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    text = models.CharField(max_length=250)
+
+
+    date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.text
