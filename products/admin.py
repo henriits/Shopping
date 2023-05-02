c2f22949eadbdf452ae1e3891ff1044587e19b74
henriits <121551949+henriits@users.noreply.github.com>
2023-05-02 21:20:30 +0300

    register items model

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from products.models import Post
+from products.models import Post, Item
 
 admin.site.register(Post)
+admin.site.register(Item)
 
