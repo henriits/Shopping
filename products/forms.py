60c7995b1f6ba28d7f342747fabd2166110784ce
henriits <121551949+henriits@users.noreply.github.com>
2023-05-02 20:57:34 +0300

    create forms

---
+++ products/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Post
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Enter a valid email.adress')
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
