32f2238b92170b83558e90d155bb5831a2646264
henriits <121551949+henriits@users.noreply.github.com>
2023-05-05 20:22:21 +0300

    remove items model ( not required yet )

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from products.models import Post, Item
+from products.models import Post
 
 admin.site.register(Post)
-admin.site.register(Item)
+
 
