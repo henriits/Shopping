d622bc1658cecddbc40fda1cc29bd88b3e9ce957
henriits <121551949+henriits@users.noreply.github.com>
2023-05-05 20:20:34 +0300

    remove Images model. import sorl.thumbnail for iMAGEPOSTING, add price and image fields to Post

--- products/models.py
+++ products/models.py
@@ -1,27 +1,20 @@
 from django.db import models
 
+from sorl.thumbnail import ImageField
+
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     text = models.CharField(max_length=250)
+    price = models.CharField(max_length=250)
+    image = ImageField()
 
-
-    date = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
-        return self.text
-
-
-
-class Item(models.Model):
-    name = models.CharField(max_length=250)
-    description = models.CharField(max_length=250)
-    price = models.CharField(max_length=250)
-
-    date = models.DateTimeField(auto_now=True)
+        return self.title
 
 
 
