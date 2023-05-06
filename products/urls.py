3dfec223fdaf4391e39f2834ab77fae10c1136a7
henriits <121551949+henriits@users.noreply.github.com>
2023-05-06 21:36:41 +0300

    created detailed view path for each item

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import *
 
+app_name = "products"  # this will be used in links to define ex: products:details
+
 urlpatterns = [
     path("",HomeView.as_view(), name="home"),
     path("products/", ProductsView.as_view(), name="products"),
     path("accounts/signup/", UserCreateView.as_view(), name="signup"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="detail")
 ]
