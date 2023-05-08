bad209cfdafb0fe035d64147248f074a95159bff
henriits <121551949+henriits@users.noreply.github.com>
2023-05-08 20:14:00 +0300

    add staticfiles_dirs to settings

--- shopping_paradise/settings.py
+++ shopping_paradise/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -137,3 +138,5 @@ SIGNUP_REDIRECT_URL = "/login/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
+
+
