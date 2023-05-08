92df4e5df16e3d84eb01e4bd85e3e66df08fb693
henriits <121551949+henriits@users.noreply.github.com>
2023-05-08 20:12:15 +0300

    AllProductsView created

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView
 
@@ -15,10 +15,21 @@ class ProductsView(ListView):
     success_url = reverse_lazy("home")
     context_object_name = "posts"
 
+class AllProductsView(ListView):
+    model = Post
+    template_name = "products/allproducts.html"
+    success_url = reverse_lazy("home")
+    context_object_name = "posts"
+
 
 
 
 class ProductDetailView(DetailView):
     http_method_names = ["get"]
     template_name = "products/detail.html"
+    model = Post
+
+
+class ShoppingCartView(ListView, LoginRequiredMixin):
+    template_name = "products/shoppingcart.html"
     model = Post
